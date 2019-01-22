4f85b6c10cb2424f15839e321f5a8eca55f29f8b
Eric Gmutza <egmutza@fusionalliance.com>
2019-01-22 14:40:08 -0500

    Add mc.exacttarget.com to allowed domains whitelist (#6)
    * Fix stack check and frame attributes
    
    * Add mc.exacttarget.com to allowed domains whitelist

--- blocksdk.js
+++ blocksdk.js
@@ -122,7 +122,7 @@ SDK.prototype.setSuperContent = function (content, cb) {
 SDK.prototype.triggerAuth = function (appID) {
 	this.getUserData(function (userData) {
 		var stack = userData.stack;
-		if (stack.startsWith('qa')) {
+		if (stack.indexOf('qa') === 0) {
 			stack = stack.substring(3,5) + '.' + stack.substring(0,3);
 		}
 		var iframe = document.createElement('IFRAME');
@@ -132,7 +132,8 @@ SDK.prototype.triggerAuth = function (appID) {
 		iframe.style.position = 'absolute';
 		iframe.style.top = '0';
 		iframe.style.left = '0';
-		iframe.style.display = 'hidden';
+		iframe.style.visibility = 'hidden';
+		iframe.className = 'authframe';
 		document.body.appendChild(iframe);
 	});
 };
@@ -185,12 +186,13 @@ SDK.prototype._receiveMessage = function _receiveMessage (message) {
 // the custom block should verify it is being called from the marketing cloud
 SDK.prototype._validateOrigin = function _validateOrigin (origin) {
 	// Make sure to escape periods since these strings are used in a regular expression
-	var allowedDomains = this._whitelistOverride || ['marketingcloudapps\\.com', 'blocktester\\.herokuapp\\.com'];
+	var allowedDomains = this._whitelistOverride || ['exacttarget\\.com', 'marketingcloudapps\\.com', 'blocktester\\.herokuapp\\.com'];
 
 	for (var i = 0; i < allowedDomains.length; i++) {
 		// Makes the s optional in https
 		var optionalSsl = this._sslOverride ? '?' : '';
-		var whitelistRegex = new RegExp('^https' + optionalSsl + '://([a-zA-Z0-9-]+\\.)*' + allowedDomains[i] + '(:[0-9]+)?$', 'i');
+		var mcSubdomain = allowedDomains[i] === 'exacttarget\\.com' ? 'mc\\.' : '';
+		var whitelistRegex = new RegExp('^https' + optionalSsl + '://' + mcSubdomain + '([a-zA-Z0-9-]+\\.)*' + allowedDomains[i] + '(:[0-9]+)?$', 'i');
 
 		if (whitelistRegex.test(origin)) {
 			return true;
